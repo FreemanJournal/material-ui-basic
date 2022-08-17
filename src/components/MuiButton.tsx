a263ca7833527156e3e490de962f54ce7a48bda3
Md Ishaque <iamishaque.business@gmail.com>
2022-08-17 10:03:45 +0600

    Add Button and Typography

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,35 @@
+import { FormatBold, FormatBoldOutlined, FormatItalic, FormatUnderlined } from '@mui/icons-material'
+import { Button, Stack, ToggleButton, ToggleButtonGroup } from '@mui/material'
+import React, { MouseEvent, useState } from 'react'
+
+export default function MuiButton() {
+    const [textFormat, setTextFormat] = useState<string | null>(null);
+    // const [textFormat, setTextFormat] = useState<string[]>([]);
+    console.log('textFormat', textFormat)
+    const handleTextFormatChange = (event: MouseEvent<HTMLElement>, updatedFormats: string | null) => {
+        setTextFormat(updatedFormats)
+    }
+    return (
+        <Stack spacing={3} direction="row">
+            <Button variant='contained' color='info'>Tap me hard</Button>
+            <Button variant='outlined' size='small' >Tap me hard</Button>
+            <ToggleButtonGroup
+                aria-label='text formatting'
+                value={textFormat}
+                onChange={handleTextFormatChange}
+                exclusive
+                >
+                <ToggleButton value="bold" aria-label="bold">
+                    <FormatBold />
+                </ToggleButton>
+                <ToggleButton value="italic" aria-label="italic">
+                    <FormatItalic />
+                </ToggleButton>
+                <ToggleButton value="underline" aria-label="underline">
+                    <FormatUnderlined />
+                </ToggleButton>
+            </ToggleButtonGroup>
+
+        </Stack>
+    )
+}
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function MuiTypography() {
+  return (
+    <div>
+        <Typography variant='h1'  component="li" gutterBottom>Overline</Typography>
+    
+    </div>
+  )
+}
